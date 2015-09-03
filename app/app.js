b5a5daf2824ccbce6405cc7b43dd4071200b73e3
Matthew Villwock <mtvillwock@gmail.com>
2015-09-02 17:43:23 -0700

    baffled

--- app/app.js
+++ app/app.js
@@ -1,48 +1,75 @@
 angular
-    .module("JHO", [
-        'ui.router'
-    ])
+    .module("JHO", ['ui.router'])
 
 .run(
     ['$rootScope', '$state', '$stateParams',
         function($rootScope, $state, $stateParams) {
 
-            // It's very handy to add references to $state and $stateParams to the $rootScope
-            // so that you can access them from any scope within your applications.For example,
+            // Add references to $state and $stateParams to the $rootScope
+            // so that you can access them from any scope within the app.For example,
             // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
             // to active whenever 'contacts.list' or one of its descendants is active.
             $rootScope.$state = $state;
             $rootScope.$stateParams = $stateParams;
+            $rootScope.$on("$stateChangeError", console.log.bind(console));
+
+            $state.transitionTo('welcome');
         }
     ]
 )
 
-.config(['$urlRouterProvider', '$stateProvider',
-    function($urlRouterProvider, $stateProvider) {
-        $urlRouterProvider.otherwise('/');
+.config(['$urlRouterProvider', '$stateProvider', '$httpProvider',
+    function($urlRouterProvider, $stateProvider, $httpProvider) {
+
+        $httpProvider.defaults.headers.common({
+            "Accept": "application/json, text/plain, * / *",
+            "Content-Type": "application/json"
+        });
 
         $stateProvider
         // Welcome state
         .state('welcome', {
             url: '/',
             templateUrl: "/templates/welcome.html",
-            controller: 'welcomeCtrl'
+            controller: 'welcomeCtrl',
+            data {
+                requireLogin: false
+            }
         })
 
-        .state('about', {
-            url: '/about',
-            templateUrl: '/templates/about.html',
-            controller: 'aboutCtrl'
+        .state('app', {
+            abstract: true,
+            // ...
+            data: {
+                requireLogin: true // this property will apply to all children of 'app'
+            }
         })
+            .state('app.dashboard', {
+                // child state of `app`
+                // requireLogin === true
+            })
 
-//         // Register state
-//         .state('register', {
-//             url: '/register',
-//             templateUrl: "/register.html",
-//             controller: function($scope) {
-//                 $scope.pageTitle = "Register New User";
-//             }
-//         })
+        // .state('about', {
+        //     url: '/about',
+        //     templateUrl: '/templates/about.html',
+        //     controller: 'aboutCtrl'
+        // })
+
+        // .state('contact', {
+        //     url: '/about',
+        //     templateUrl: '/templates/contact.html',
+        //     controller: 'contactCtrl'
+        // })
+
+        //         // Register state
+        //         .state('register', {
+        //             url: '/register',
+        //             templateUrl: "/register.html",
+        //             controller: function($scope) {
+        //                 $scope.pageTitle = "Register New User";
+        //             }
+        //         })
+        $urlRouterProvider.otherwise('/');
     }
 ])
 
