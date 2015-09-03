48e5e962a23b4bc3567545d453c684b02d29c590
Matthew Villwock <mtvillwock@gmail.com>
2015-09-02 22:27:13 -0700

    rewrite with ngRoute and ng-view

--- app/app.js
+++ app/app.js
@@ -1,123 +1,46 @@
 angular
-    .module("JHO", ['ui.router'])
+    .module("JHO", ['ngRoute', 'ngResource'])
 
-.run(
-    ['$rootScope', '$state', '$stateParams',
-        function($rootScope, $state, $stateParams) {
-
-            // Add references to $state and $stateParams to the $rootScope
-            // so that you can access them from any scope within the app.For example,
-            // <li ng-class="{ active: $state.includes('contacts.list') }"> will set the <li>
-            // to active whenever 'contacts.list' or one of its descendants is active.
-            $rootScope.$state = $state;
-            $rootScope.$stateParams = $stateParams;
-            $rootScope.$on("$stateChangeError", console.log.bind(console));
-
-            $state.transitionTo('welcome');
-        }
-    ]
-)
-
-.config(['$urlRouterProvider', '$stateProvider', '$httpProvider',
-    function($urlRouterProvider, $stateProvider, $httpProvider) {
-
+.config([
+    // These strings are annotations of dependencies being injected
+    // This is for properly tracking dependencies in minified code
+    '$httpProvider', '$routeProvider',
+    function($httpProvider, $routeProvider) {
+        // setting headers for CORS requests
         $httpProvider.defaults.headers.common({
             "Accept": "application/json, text/plain, * / *",
             "Content-Type": "application/json"
         });
 
-        $stateProvider
-        // Welcome state
-        .state('welcome', {
-            url: '/',
-            templateUrl: "/templates/welcome.html",
-            controller: 'welcomeCtrl',
-            data {
-                requireLogin: false
+        // If client doesn't hit an existing route, go to '/'
+        $routeProvider.otherwise('/');
+        // if client hits index, send them to welcome page
+        $routeProvider.when('/', {
+            redirectTo: '/welcome'
+        });
+
+        // Welcome view
+        $routeProvider.when('/welcome', {
+            templateUrl: "templates/welcome.html",
+            controller: 'WelcomeController',
+            controllerAs: 'welcomeCtrl'
+        });
+
+        // Register view
+        $routeProvider.when('/register', {
+            templateUrl: "templates/register.html",
+            controller: function($scope) {
+                $scope.pageTitle = "Register New User";
             }
-        })
+        });
 
-        .state('app', {
-            abstract: true,
-            // ...
-            data: {
-                requireLogin: true // this property will apply to all children of 'app'
+        // Login view
+        $routeProvider.when('/login', {
+            templateUrl: "templates/register.html",
+            controller: function($scope) {
+                $scope.pageTitle = "Register New User";
             }
-        })
-            .state('app.dashboard', {
-                // child state of `app`
-                // requireLogin === true
-            })
+        });
 
-        // .state('about', {
-        //     url: '/about',
-        //     templateUrl: '/templates/about.html',
-        //     controller: 'aboutCtrl'
-        // })
-
-        // .state('contact', {
-        //     url: '/about',
-        //     templateUrl: '/templates/contact.html',
-        //     controller: 'contactCtrl'
-        // })
-
-        //         // Register state
-        //         .state('register', {
-        //             url: '/register',
-        //             templateUrl: "/register.html",
-        //             controller: function($scope) {
-        //                 $scope.pageTitle = "Register New User";
-        //             }
-        //         })
-        $urlRouterProvider.otherwise('/');
     }
 ])
-
-// .config([
-//     // These strings are annotations of dependencies being injected
-//     // This is for properly tracking dependencies in minified code
-//     '$urlRouterProvider',
-//     '$stateProvider',
-//     '$httpProvider',
-//     function($urlRouterProvider, $stateProvider, $httpProvider) {
-//         // $httpProvider.defaults.headers.common({
-//         //     "Accept": "application/json, text/plain, * / *",
-//         //     "Content-Type": "application/json"
-//         // });
-
-//         // If client doesn't hit an existing route, go to '/'
-//         $urlRouterProvider.otherwise('/');
-//         // $urlRouterProvider.when('/foo', {
-//         //     redirectTo: '/'
-//         // });
-
-//         // States are comprised of a state name and a configuration object
-//         // Common config options include:
-//         // url => the url of the state
-//         // template || templateUrl || templateProvider => the HTML for that state
-//         // controller => define values for {{ expressions }} in HTML
-//         // ===> Can also reference an already defined controller
-//         // controllerAs => specify alias for controller, e.g., 'ItemController as item'
-//         $stateProvider
-
-//         // Welcome state
-//         .state('welcome', {
-//             url: '/',
-//             templateUrl: "/templates/welcome.html",
-//             controller: 'WelcomeController',
-//             controllerAs: 'welcomeCtrl'
-//         })
-
-//         // Register state
-//         .state('register', {
-//             url: '/register',
-//             templateUrl: "/register.html",
-//             controller: function($scope) {
-//                 $scope.pageTitle = "Register New User";
-//             }
-//         })
-
-//         // Login state
-
-//     }
-// ])
