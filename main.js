b93baddae7711ed3ffd24ed1375fa0b77a459ef4
rilkevb <rilkevb@gmail.com>
2015-09-04 15:58:02 -0700

    Refactored board displaying from properly nested js object in single controller.  Committing before adding drag/drop

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 (function() {
-  var app = angular.module("jho",[]);
+  var app = angular.module("jho",['dndLists']);
 
   app.controller("PanelController", function(){
     this.tab = 1;
@@ -15,12 +15,12 @@
 
   app.controller('BoardController',function(){
     this.board = board1;
-    this.list1_items = list1_items;
+    // this.list1_items = board1.lists[0];
   });
 
   app.controller("AddOrganizationController", function(){
     this.organization = {};
-    this.list1_items = list1_items;
+    this.list1_items = board1.lists;
 
     this.addOrganization = function(list1_items) {
       this.list1_items.push(this.organization);
@@ -28,15 +28,44 @@
     };
   });
 
+
   var board1 = {
-    name: "Test Board Title",
-    description: '...'
+    name: "New Test Board",
+    lists:[
+        {
+            name: "Interested",
+            cards: [
+                {title: 'Google'},
+                {title: 'FaceBook'}
+            ]
+        },
+        {
+            name: "Applied",
+            cards: [
+                {title: 'Wired'},
+                {title: 'Make'}
+            ]
+        },
+        {
+            name: "Interviewed",
+            cards: [
+                {title: 'Wired'},
+                {title: 'Make'}
+            ]
+        }
+    ],
+    user_id: 123456
   }
-  var list1_items = [
-    { title: "Google"},
-    { title: "Wired"},
-    { title: "TheGreatBlue"},
-  ]
+
+  // var board1 = {
+  //   name: "Test Board Title",
+  //   description: '...'
+  // }
+  // var list1_items = [
+  //   { title: "Google"},
+  //   { title: "Wired"},
+  //   { title: "TheGreatBlue"},
+  // ]
 
 })();
 
