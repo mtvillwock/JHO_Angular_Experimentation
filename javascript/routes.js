8b1cddb8e30329b95894259ecf539d279bfcc5b7
rilkevb <rilkevb@gmail.com>
2015-09-07 21:29:39 -0700

    Add activetab to routes.js

--- javascript/routes.js
+++ javascript/routes.js
@@ -4,19 +4,24 @@ angular.module('jho')
   function($routeProvider) {
     $routeProvider
     .when('/',{
-      templateUrl: '/templates/panels/board/board-index.html'
+      templateUrl: '/templates/panels/board/board-index.html',
+      activetab: 'board'
     })
     .when('/board',{
-      templateUrl: '/templates/panels/board/board-index.html'
+      templateUrl: '/templates/panels/board/board-index.html',
+      activetab: 'board'
     })
     .when('/today',{
-      templateUrl: '/templates/panels/today/today-index.html'
+      templateUrl: '/templates/panels/today/today-index.html',
+      activetab: 'today'
     })
     .when('/stats',{
-      templateUrl: '/templates/panels/stats/stats-index.html'
+      templateUrl: '/templates/panels/stats/stats-index.html',
+      activetab: 'today'
     })
     .when('/tips',{
-      templateUrl: '/templates/panels/tips/tips-index.html'
+      templateUrl: '/templates/panels/tips/tips-index.html',
+      activetab: 'board'
     })
     .otherwise( {redirectTo: '/' });
   }
