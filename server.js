665be3c4752669904254129e05a054d60dfd6574
Matthew Villwock <mtvillwock@gmail.com>
2015-09-13 19:14:12 -0700

    fix port for heroku

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
 var app = express();
 app.use('/', express.static(__dirname + '/'));
-app.listen(5000, function() { console.log('listening')});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+    console.log('Our app is running on: ' + port);
+});
