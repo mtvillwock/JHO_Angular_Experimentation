5451752b1e7ea112274c620af383553dcf0b84bd
Matthew Villwock <mtvillwock@gmail.com>
2015-09-15 15:11:04 -0700

    fix card missing id when added

--- javascripts/app.js
+++ javascripts/app.js
@@ -8,137 +8,148 @@ angular.module("JHO", ['ngRoute', 'ngResource', 'dndLists'])
     }
 ])
 
-.controller('BoardController', ['$http','$scope','API', function($http,$scope, API) {
-    $scope.board = {};
-    $scope.list1_items = [];
-    $scope.allCards = [];
-    // $scope
+.controller('BoardController', ['$http', '$scope', 'API',
+    function($http, $scope, API) {
+        $scope.board = {};
+        $scope.list1_items = [];
+        $scope.allCards = [];
+        // $scope
 
-    // Refactor this into a service later
-    (function(){
-        $http({method: 'GET', url: API+'/dashboard'})
-        .success(function(returnValues){
-            // console.log("ReturnValues : ", returnValues)
-            $scope.board = returnValues.board;
-            console.log("Inside succes:", $scope.board);
-            $scope.list1_items = $scope.board.lists[0]
-            $scope.allCards = returnValues.cards;
-            // console.log("All cards: ", $scope.allCards)
-            // console.log("Entire board: ", returnValues)
-        });
-    })();
+        // Refactor this into a service later
+        (function() {
+            $http({
+                method: 'GET',
+                url: API + '/dashboard'
+            })
+                .success(function(returnValues) {
+                    // console.log("ReturnValues : ", returnValues)
+                    $scope.board = returnValues.board;
+                    console.log("Inside succes:", $scope.board);
+                    $scope.list1_items = $scope.board.lists[0]
+                    $scope.allCards = returnValues.cards;
+                    // console.log("All cards: ", $scope.allCards)
+                    // console.log("Entire board: ", returnValues)
+                });
+        })();
 
 
-    this.updateCardPosition = function(event,index,card, list_id) {
-        console.log("in updateCardPosition")
-        console.log("event,index,card, list_id", event,index,card, list_id);
+        this.updateCardPosition = function(event, index, card, list_id) {
+            console.log("in updateCardPosition")
+            console.log("event,index,card, list_id", event, index, card, list_id);
 
-        card.list_id = list_id; // update Card in DOM
+            card.list_id = list_id; // update Card in DOM
 
-        $http.post( API+'/cards/' + card.id + '/movements', {
-            card: {
-                title: card.title,
-                list_id: list_id
-            }
-        })
-        .success(function(response){
-            // response.movement and response.card
-            console.log("moved card: ", response);
-            card.priority = response.card.priority;
-            console.log("card with new priority is: ", card);
-        });
-    };
+            $http.post(API + '/cards/' + card.id + '/movements', {
+                card: {
+                    title: card.title,
+                    list_id: list_id
+                }
+            })
+                .success(function(response) {
+                    // response.movement and response.card
+                    console.log("moved card: ", response);
+                    card.priority = response.card.priority;
+                    console.log("card with new priority is: ", card);
+                });
+        };
 
-    // this.remainingTasks = function
+        // this.remainingTasks = function
 
-    // $scope.dropCallback = function(event, index, item, external, type, allowedType) {
-    //     console.log("dropped item is: ", item);
-    //     $scope.logListEvent('dropped at', event, index, external, type);
-    //     if (external) {
-    //         if (allowedType === 'itemType' && !item.label) return false;
-    //         if (allowedType === 'containerType' && !angular.isArray(item)) return false;
-    //     }
-    //     return item;
-    // };
+        // $scope.dropCallback = function(event, index, item, external, type, allowedType) {
+        //     console.log("dropped item is: ", item);
+        //     $scope.logListEvent('dropped at', event, index, external, type);
+        //     if (external) {
+        //         if (allowedType === 'itemType' && !item.label) return false;
+        //         if (allowedType === 'containerType' && !angular.isArray(item)) return false;
+        //     }
+        //     return item;
+        // };
 
-    // $scope.logListEvent = function(action, event, index, external, type) {
-    //     var message = external ? 'External ' : '';
-    //     message += type + ' element is ' + action + ' position ' + index;
-    //     console.log(message, event);
-    // };
+        // $scope.logListEvent = function(action, event, index, external, type) {
+        //     var message = external ? 'External ' : '';
+        //     message += type + ' element is ' + action + ' position ' + index;
+        //     console.log(message, event);
+        // };
 
-}])
+    }
+])
 
-.controller("AddOrganizationController", ['$scope','$http','API', function($scope,$http,API) {
-    this.organization = {};
-    // this.list1_items = board1.lists[0];
-    var newCard = function(card, list_id){
-
-        $http.post( API+'/cards', {
-            card: {
-                title: card.title,
-                list_id: list_id
-            }
-        })
-        .success(function(returnValues){
-            // This needs to set the ID of the Card so that we can use the dropCallback that needs the card ID
-            console.log("ReturnValues : ", returnValues); // ReturnValues :  Object {id: 56, list_id: 160, organization_name: null, organization_summary: null, position_applied_for: null…}
-            // card.id = returnValues.id;
-            // console.log(card); //Object {title: "Asana", $$hashKey: "object:167", id: 56}
-            // $scope.board = returnValues.board;
-            console.log("Inside succes:", $scope.board);
-            // $scope.list1_items = $scope.board.lists[0]
-        });
-    };
-
-    this.addOrganization = function(list1_items) {
-        console.log("in AddOrganizationController", $scope.board)
-        $scope.list1_items.cards.push(this.organization);
+.controller("AddOrganizationController", ['$scope', '$http', 'API',
+    function($scope, $http, API) {
         this.organization = {};
+        // this.list1_items = board1.lists[0];
+        var newCard = function(card, list_id) {
 
-        var card = $scope.list1_items.cards[$scope.list1_items.cards.length - 1];
-        var list_id = $scope.list1_items.id
-        console.log("Card", card)
-        newCard(card, list_id);
+            $http.post(API + '/cards', {
+                card: {
+                    title: card.title,
+                    list_id: list_id
+                }
+            })
+                .success(function(card) {
+                    // This needs to set the ID of the Card so that we can use the dropCallback that needs the card ID
+                    console.log("Cards#create returns: ", card);
 
-    };
+                    $scope.list1_items.cards.pop();
+                    $scope.list1_items.cards.push(card);
 
+                    // $scope.board = returnValues.board;
+                    // console.log("Inside succes:", $scope.board);
+                    // $scope.list1_items = $scope.board.lists[0]
+                });
+        };
 
-}])
+        this.addOrganization = function(list1_items) {
+            console.log("in AddOrganizationController", $scope.board)
+            // reset addOrgCtrl.organization ng-model
+            $scope.list1_items.cards.push(this.organization);
+            this.organization = {};
+
+            var card = $scope.list1_items.cards[$scope.list1_items.cards.length - 1];
+            console.log("card to send to DB:", card);
+            var list_id = $scope.list1_items.id
+
+            return newCard(card, list_id);
+        };
+
+    }
+])
 
 //Removed card from the cardController function call...
-.controller('CardController', ['$scope','$http','API', function($scope,$http,API) {
-    console.log("in CardController")
-    $scope.modalShown = false;
-    $scope.toggleModal = function() {
-        // console.log("Toggle modal!!!")
-        $scope.modalShown = !$scope.modalShown;
-        // console.log($scope.modalShown);
-    };
+.controller('CardController', ['$scope', '$http', 'API',
+    function($scope, $http, API) {
+        console.log("in CardController")
+        $scope.modalShown = false;
+        $scope.toggleModal = function() {
+            // console.log("Toggle modal!!!")
+            $scope.modalShown = !$scope.modalShown;
+            // console.log($scope.modalShown);
+        };
 
-    this.updateCard = function(card) {
-        console.log("In updateCard function:", card)
-        // Add in all card attributes, eventually
-        $http.put( API+'/cards/' + card.id, {
-            card: {
-                title: card.title,
-                description: card.description,
-                organization_name: card.organization_name,
-                organization_summary: card.organization_summary,
-                position_description: card.position_description,
-                advocate: card.advocate,
-                position_applied_for: card.position_applied_for,
-                tech_stack: card.tech_stack,
-                glassdoor_rating: card.glassdoor_rating,
-                recent_articles: card.recent_articles
-            }
-        })
-        .success(function(response){
-            console.log("updated card: ", response);
-        });
+        this.updateCard = function(card) {
+            console.log("In updateCard function:", card)
+            // Add in all card attributes, eventually
+            $http.put(API + '/cards/' + card.id, {
+                card: {
+                    title: card.title,
+                    description: card.description,
+                    organization_name: card.organization_name,
+                    organization_summary: card.organization_summary,
+                    position_description: card.position_description,
+                    advocate: card.advocate,
+                    position_applied_for: card.position_applied_for,
+                    tech_stack: card.tech_stack,
+                    glassdoor_rating: card.glassdoor_rating,
+                    recent_articles: card.recent_articles
+                }
+            })
+                .success(function(response) {
+                    console.log("updated card: ", response);
+                });
 
-    };
-}])
+        };
+    }
+])
 
 // .controller('TodayController', function(){
 //     console.log("today controller");
