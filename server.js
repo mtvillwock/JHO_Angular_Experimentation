57bb55b4a6b2a638320edc0b7bbef15773c6b6f7
Matthew Villwock <mtvillwock@gmail.com>
2016-01-14 19:52:08 -0800

    fix refresh issue to always go to index

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 app.use('/', express.static(__dirname + '/'));
+
+app.get('*', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+})
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log('Our app is running on: ' + port);
